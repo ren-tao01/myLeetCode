4d700062ed72b4abc0d47b7ed62342c2d4ad2da2
ren-tao01 <rentao_326@hotmail.com>
2022-10-09 19:34:34 +0100

    797. All Paths From Source to Target

--- lc_797.py
+++ lc_797.py
@@ -1,29 +1,24 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        nsize = len(graph)
-        self.allPaths = list()
-
-        count = 0
-        for node in graph:
-            
-            for n in node:
-                path = list()
-                path.append(count)
-                path.append(n)
-                for g in graph.index(n):
-                    temp = path.append(g)
-                    g = temp
-
-            count +=1
-
-
-
-
-        return self.allPaths
-
-
+        #edges cases:
+        if not graph:
+            return []
         
-            
-
-        
-            
+        # build di-graph - dictionary
+        d = {}
+        for i in range(len(graph)):
+            d[i] = graph[i] # one-way link
+        print(d)
+        # apply DFS on DAG
+        n = len(graph)
+        stack = [(0, [0])] # - store both the (node, and the path leading to it)
+        res = []
+        while stack:
+            node, path = stack.pop()
+            # check leaf
+            if node == n - 1:
+                res.append(path)
+            # traverse rest
+            for nex in d[node]:
+                stack.append((nex, path+[nex]))
+        return res
